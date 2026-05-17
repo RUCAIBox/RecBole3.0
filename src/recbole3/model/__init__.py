320772d5e7572ed06514c4dfef7aceb361aa1e2e
xujiale-202308010418 <xjl627@hnu.edu.cn>
2026-05-17 16:49:40 +0800

    Add ETEGRec test suite and config

--- src/recbole3/model/__init__.py
+++ src/recbole3/model/__init__.py
@@ -12,6 +12,13 @@ from recbole3.model.base import (
     ModelConfig,
     ModelDatasets,
 )
+from recbole3.model.etegrec import (
+    ETEGRecConfig,
+    ETEGRecModel,
+    ETEGRecModelDataset,
+    ETEGRecTrainer,
+    ETEGRecTrainerConfig,
+)
 from recbole3.model.hstu import (
     HISTORY_TIMESTAMPS,
     HSTUConfig,
@@ -83,6 +90,14 @@ MODEL_TABLE: dict[str, ModelSpec] = {
         trainer_config_cls=TrainerConfig,
         pipeline_cls=Pipeline,
     ),
+    "etegrec": ModelSpec(
+        model_cls=ETEGRecModel,
+        config_cls=ETEGRecConfig,
+        model_data_cls=ETEGRecModelDataset,
+        trainer_cls=ETEGRecTrainer,
+        trainer_config_cls=ETEGRecTrainerConfig,
+        pipeline_cls=Pipeline,
+    ),
     "rqvae": ModelSpec(
         model_cls=RQVAEModel,
         config_cls=RQVAEConfig,
@@ -145,6 +160,11 @@ __all__ = [
     "BaseRankingModel",
     "BaseRetrievalModel",
     "BaseSequentialModelDataset",
+    "ETEGRecConfig",
+    "ETEGRecModel",
+    "ETEGRecModelDataset",
+    "ETEGRecTrainer",
+    "ETEGRecTrainerConfig",
     "HISTORY_ITEM_IDS",
     "HISTORY_TIMESTAMPS",
     "HSTUConfig",
